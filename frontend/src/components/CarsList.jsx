fa5c3209ef97ae4c2a0acb052ad6f72e90cca6b9
ZielinskiSzymon <zielinskiszymon89@gmail.com>
2026-05-20 17:52:15 +0200

    New look of car cards

--- frontend/src/components/CarsList.jsx
+++ frontend/src/components/CarsList.jsx
@@ -2,7 +2,7 @@ import CarCard from './CarCard'
 
 export default function CarsList({ cars, user, onRent, onSelectCar }) {
 	return (
-		<div className='row row-cols-1 row-cols-sm-2 row-cols-md-3 g-4'>
+		<div className='row row-cols-sm-1 row-cols-md-2 row-cols-lg-3 '>
 			{cars.map((car) => (
 				<CarCard key={car.id} car={car} user={user} onRent={onRent} onSelectCar={onSelectCar}/>
 			))}
